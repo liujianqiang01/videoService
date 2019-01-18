fa281c8deba874c866c55e63ac2fa9cca70ab013
liujianqiang <liujianqiang@yoho8.com>
2019-01-18 13:06:09 +0800

    帮助

---
+++ src/main/java/com/video/util/DateUtils.java
@@ -0,0 +1,45 @@
+package com.video.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * @Author: liujianqiang
+ * @Date: 2019-01-18
+ * @Description:
+ */
+public class DateUtils {
+
+    //获取当前月第一天
+    public static String getThisMounthFirstDay() {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.MONTH, 0);
+        c.set(Calendar.DAY_OF_MONTH, 1);//设置为1号,当前日期既为本月第一天
+        String first = format.format(c.getTime());
+        return first;
+    }
+    //获取当前月最后一天
+    public static String getThisMounthLastDay() {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar ca = Calendar.getInstance();
+        ca.set(Calendar.DAY_OF_MONTH, ca.getActualMaximum(Calendar.DAY_OF_MONTH));
+        String last = format.format(ca.getTime());
+        return last;
+    }
+
+    /**
+     * 字符串的日期格式的计算
+     */
+    public static int daysBetween(String smdate, String bdate) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(sdf.parse(smdate));
+        long time1 = cal.getTimeInMillis();
+        cal.setTime(sdf.parse(bdate));
+        long time2 = cal.getTimeInMillis();
+        long between_days = (time2 - time1) / (1000 * 3600 * 24);
+        return Integer.parseInt(String.valueOf(between_days));
+    }
+}
